e62793a3280df3bbaeb1a0c46bb8a4a3c1a32b24
Manuel Lopera <manuel.ospina@truepill.com>
2022-01-03 10:23:53 -0500

    fix

--- contentful_migrations/2.js
+++ contentful_migrations/2.js
@@ -1,7 +1,7 @@
 module.exports = function runMigration(migration) {
   const dog = migration.createContentType('dog', {
     name: 'Dog',
-    description: 'Test description for a Dog'
+    description: 'Test description for a Dog model'
   });
   dog.createField('name').name('name').type('Symbol').required(false);
 };
